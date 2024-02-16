302d9fc5f86bee6c161ff8f535ce94629b1b8c0e
Maksim Tretiakov <m.tretyakov@kefirgames.com>
2024-02-16 13:14:23 +0300

    upd: scene selector

--- Editor/SceneSelector/SceneSelectorWindow.cs
+++ Editor/SceneSelector/SceneSelectorWindow.cs
@@ -3,60 +3,102 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Editor.SceneSelector
 {
     public class SceneSelectorWindow : EditorWindow
     {
+        private const string SearchFieldLabel = "Search:";
+        private const string WindowTitle = "Scene Selector";
+        private const string SceneAssetType = "t:Scene";
+        private const string AssetsFolderPrefix = "Assets/";
+        private const int WindowWidth = 260;
+        private const int WindowHeight = 400;
+        private const int FolderLabelFontSize = 13;
+        private const int SceneLabelWidth = 200;
+        private const float ButtonGroupMarginLeft = 3f;
+
         private TextField _searchField;
-        private VisualElement _listContainer;
+        private ScrollView _listContainer;
 
         [MenuItem("Tools/Scene Selector %`")]
         public static void Init()
         {
-            GetWindowWithRect(typeof(SceneSelectorWindow), new Rect(0, 0, 260, 400), false, "Scene Selector");
+            var window = GetWindowWithRect<SceneSelectorWindow>(new Rect(0, 0, WindowWidth, WindowHeight), false, WindowTitle);
+            window.Show();
         }
 
         public void CreateGUI()
         {
-            _searchField = new TextField("Search:");
+            CreateSearchField();
+            CreateListContainer();
+            UpdateSceneList("");
+        }
+
+        private void CreateSearchField()
+        {
+            _searchField = new TextField(SearchFieldLabel);
             _searchField.RegisterValueChangedCallback(evt => UpdateSceneList(evt.newValue));
             rootVisualElement.Add(_searchField);
+        }
 
-            _listContainer = new ScrollView();
-            _listContainer.style.height = 300;
+        private void CreateListContainer()
+        {
+            _listContainer = new ScrollView { style = { height = WindowHeight } };
             rootVisualElement.Add(_listContainer);
-
-            UpdateSceneList("");
         }
 
         private void UpdateSceneList(string searchText)
         {
             _listContainer.Clear();
-            var sceneGuids = AssetDatabase.FindAssets("t:Scene");
+            var scenesByFolder = GetScenesOrganizedByFolder();
+            DisplayScenesByFolder(scenesByFolder, searchText.ToLower());
+        }
+
+        private Dictionary<string, List<string>> GetScenesOrganizedByFolder()
+        {
+            var sceneGuids = AssetDatabase.FindAssets(SceneAssetType)
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Where(path => path.StartsWith(AssetsFolderPrefix))
+                .ToList();
 
             var scenesByFolder = new Dictionary<string, List<string>>();
 
-            foreach (var sceneGuid in sceneGuids)
+            foreach (var scenePath in sceneGuids)
             {
-                var scenePath = AssetDatabase.GUIDToAssetPath(sceneGuid);
-                var folderPath = System.IO.Path.GetDirectoryName(scenePath);
+                var folderPath = Path.GetDirectoryName(scenePath)?.Split(Path.DirectorySeparatorChar).LastOrDefault();
+                if (folderPath == null) continue;
+
                 if (!scenesByFolder.ContainsKey(folderPath))
                 {
                     scenesByFolder[folderPath] = new List<string>();
                 }
-                scenesByFolder[folderPath].Add(sceneGuid);
+                scenesByFolder[folderPath].Add(scenePath);
             }
 
+            return scenesByFolder;
+        }
+
+        private void DisplayScenesByFolder(Dictionary<string, List<string>> scenesByFolder, string searchText)
+        {
             foreach (var folder in scenesByFolder)
             {
-                var folderLabel = new Label(folder.Key);
+                var folderLabel = new Label(folder.Key)
+                {
+                    style =
+                    {
+                        color = new StyleColor(Color.white),
+                        fontSize = FolderLabelFontSize
+                    }
+                };
                 _listContainer.Add(folderLabel);
 
-                foreach (var sceneGuid in folder.Value)
+                foreach (var scenePath in folder.Value)
                 {
-                    var sceneButton = CreateSceneButton(sceneGuid);
-                    if (sceneButton != null && sceneButton.Q<Label>().text.ToLower().Contains(searchText.ToLower()))
+                    var sceneButton = CreateSceneButton(scenePath);
+                    if (sceneButton != null && sceneButton.Q<Label>().text.ToLower().Contains(searchText))
                     {
                         _listContainer.Add(sceneButton);
                     }
@@ -64,32 +106,29 @@ namespace Editor.SceneSelector
             }
         }
 
-        private static VisualElement CreateSceneButton(string sceneGuid)
+        private static VisualElement CreateSceneButton(string scenePath)
         {
-            var scenePath = AssetDatabase.GUIDToAssetPath(sceneGuid);
-            var buttonGroup = new VisualElement();
-
-            buttonGroup.style.flexDirection = FlexDirection.Row;
-            buttonGroup.style.marginLeft = 3;
-
             var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
-            if (sceneAsset == null || sceneAsset.name is "Basic" or "Standard")
+            if (sceneAsset == null) return null;
+
+            var buttonGroup = new VisualElement
             {
-                return null;
-            }
-            var label = new Label($"{sceneAsset.name}");
-            label.style.width = 200;
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    marginLeft = ButtonGroupMarginLeft
+                }
+            };
+
+            var label = new Label(sceneAsset.name) { style = { width = SceneLabelWidth } };
             buttonGroup.Add(label);
 
-            var openButton = new Button(() =>
-            {
-                EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-            })
+            var openButton = new Button(() => EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single))
             {
                 text = "Open"
             };
-
             buttonGroup.Add(openButton);
+
             return buttonGroup;
         }
     }
